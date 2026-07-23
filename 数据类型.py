c12e0b1a201974d101d60d81938ce83197d2de40
lxadoh <2788639929@qq.com>
2026-07-23 20:51:06 +0800

    第一天

---
+++ 数据类型.py
@@ -0,0 +1,30 @@
+# # type() 获取数据的类型
+# print(type(100))
+# print(type(1.0))
+# print(type("study"))
+# num = "hello"
+# print(type(num))
+#
+# # isinstance(数据,类型) 检查数据是否属于指定的类型，返回的是一个bool值
+# print(isinstance(1.0, float))
+# print(isinstance(100, float))
+# a = "world"
+# print(isinstance(a, str))
+
+# 字符串
+# 字符串的定义
+a1 = "python"
+a2 = 'string'
+a3 = """
+hello:
+    this is my py-study
+"""
+print(a1)
+print(a2)
+print(a3)
+
+#转义字符 \'  \"  \n  \t
+a4='It\'s very good'
+print(a4)
+a5="It's very good"
+print(a5)
