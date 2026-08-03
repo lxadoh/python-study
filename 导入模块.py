d6434b368c31e5bfa3829320aaadeff3e88c37c4
lxadoh <2788639929@qq.com>
2026-08-03 16:13:57 +0800

    第6天

---
+++ 导入模块.py
@@ -0,0 +1,14 @@
+# # 导入模块
+import random
+import random as rd
+
+for i in range(5):
+    print(random.randint(1, 100))
+
+
+# 导入模块中的功能
+from random import randint
+from random import randint as rt
+from random import *
+
+print(randint(1,100))
